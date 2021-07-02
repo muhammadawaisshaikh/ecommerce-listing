3b297395a9aa641b01586f5a138215f2eb2e6dd0
Muhammad Awais <awaisshaikh94@gmail.com>
2021-07-02 12:33:47 +0500

    sidebar done

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  categories: any = [
+    { id: 1, name: "Beverages", slug: "beverages" },
+    { id: 2, name: "Frozen", slug: "frozen" },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
